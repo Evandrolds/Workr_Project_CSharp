03b474a6512b717d48c800015596079a66fd621e
Evandrolds <evandro.lds.ds@outlook.com>
2022-12-02 20:09:39 -0300

    Created Department class

---
+++ WorkerProject/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿
+
+namespace WorkerProject.Entities
+{
+    class Department
+    {
+        public string Name { get; set; } 
+        
+        public Department(string name)
+        {
+            this.Name = name;
+        }
+        public Department() { }
+
+        override
+            public string ToString()
+        {
+            return "  Department: " + Name;
+        }
+    }
+
+}

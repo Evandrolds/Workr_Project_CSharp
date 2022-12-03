30061776ddf00ca86e12b799c514e5b2f87687d2
Evandrolds <evandro.lds.ds@outlook.com>
2022-12-03 10:26:19 -0300

    Update Income and ToStrong methods

--- WorkerProject/Entities/Worker.cs
+++ WorkerProject/Entities/Worker.cs
@@ -9,12 +9,14 @@ class Worker
     public string Name { get; set; }
     public WorkLevel Level { get; set; }
     public double BaseSalary { get; private set; }
+    public Department department { get; set; }
     public List<HourContract> Contracts { get; set; } = new List<HourContract>();
 
 
-    public Worker(string name, WorkLevel level, double baseSalary)
+    public Worker(string name, Department department, WorkLevel level, double baseSalary)
     {
         Name = name;
+        this.department = department;
         Level = level;
         BaseSalary = baseSalary;
     }
@@ -23,32 +25,34 @@ class Worker
     {
         return " Name: " + Name +
             "\n Working level: " + Level +
+            "\n Department: " + department.Name +
             "\n Base Salary: $ " + BaseSalary.ToString("F2", CultureInfo.InvariantCulture);
     }
     public void AddContract(HourContract contract)
     {
-        if (contract != null && !Contracts.Contains(contract))
+        if (contract != null)
         {
-
             Contracts.Add(contract);
+
         }
     }
     public void RemoveContract(HourContract contract)
     {
-        if (contract != null && Contracts.Contains(contract))
+        if (contract != null)
         {
             Contracts.Remove(contract);
+
         }
     }
-    public double Income(int year, int month)
+    public double Income(int month, int year)
     {
         double sun = BaseSalary;
         foreach (HourContract obj in Contracts)
+
         {
-            if (obj.Date.Year == year && obj.Date.Month == month)
+            if (obj.Date.Month == month && obj.Date.Year == year)
             {
                 sun += obj.TotalValue();
-
             }
         }
         return sun;
